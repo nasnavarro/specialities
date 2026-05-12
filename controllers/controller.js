bb884ebd5696f96cb11cb98d4673d2259f39195b
Ignacio <inavarro@tesoriza.com>
2026-05-12 17:43:35 +0200

    Ejercicio modificando la salido JSON a HTML

--- controllers/controller.js
+++ controllers/controller.js
@@ -63,3 +63,69 @@ export const getUsersBySpecialty = (req, res) => {
   });
 };
 
+// Función que devuelve el html para la ruta index
+export const getIndexHtml = (req, res) => {
+  const especialidades = getEspecialidades();
+
+  const links = especialidades
+    .map(esp => `<li><a href="/${esp}">${esp}</a></li><br/>`)
+    .join('');
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <h1>HOME</h1>
+        <p>Elige la especialidad</p>
+        <ul>${links}</ul>
+      </body>
+    </html>
+  `);
+};
+
+// Función que devuelve el html para la ruta de una especialidad
+export const getUsersBySpecialtyHtml = (req, res) => {
+  let retval = {result:true, data: [], message:''};
+
+  //Obtenemos la especialidad recibida por parámetro
+  const especialidad = req.params.especialidad.toLowerCase();
+
+  if (!verifyUsersData(res)){
+    retval.result = false;
+    retval.message = 'No hay datos de usuario cargados';
+  }else{
+    //Obtenemos las especialidades disponibles
+    const especialidades = getEspecialidades();
+
+    //Vemos si la especialidad está en el listado de especialidades
+    if(!especialidades.includes(especialidad)){
+        retval.result = false;
+        retval.message = `La especialidad solicitada (${especialidad}), no existe en el listado de datos`;
+    }else{
+        //Si existe obtenemos los elementos de la especialidad
+        retval.data = usersData.filter((item) => item.specialty.toLowerCase() === especialidad);
+    }
+  }
+
+  let content = '';
+
+  if(retval.result){
+    content += `<p>Número de personas: ${retval.data.length}`;
+    content += retval.data
+    .map(esp => `<p><b>ID:</b> ${esp.id}<br/><b>Nombre:</b> ${esp.name}<br/><b>Edad:</b> ${esp.age}</p>`)
+    .join('');
+  }else{
+    content = retval.message;
+  }
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <h1 style="text-transform:uppercase">${especialidad}</h1>
+        ${content}
+        <p><a href="/">Volver</a></p>
+      </body>
+    </html>
+  `);
+};
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,11 @@ import * as myController from "../controllers/controller.js";
 const router = express.Router();
 
 //Página principal
-router.get('/', (req, res) => myController.getAllUsers(req,res));
+//router.get('/', (req, res) => myController.getAllUsers(req,res)); //Para JSON
+router.get('/', (req, res) => myController.getIndexHtml(req,res)); //Para HTML
 
 //Página por especialidades
-router.get("/:especialidad", (req, res) => myController.getUsersBySpecialty(req,res));
+//router.get("/:especialidad", (req, res) => myController.getUsersBySpecialty(req,res));    //Para JSON
+router.get("/:especialidad", (req, res) => myController.getUsersBySpecialtyHtml(req,res));    //Para HTML
 
 export default router;
